f0937d6864c9b3863b6f30a1468b74f9690eb484
Jeon <wjsdbtjd0225@naver.com>
2024-10-07 14:42:27 +0900

    [Update] Title Middle Save

--- Assets/3.Script/Title/BirdMove.cs
+++ Assets/3.Script/Title/BirdMove.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdMove : MonoBehaviour
+public class CloudBirdMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject clouds;
+    public GameObject bird_L;
+    public GameObject bird_R;
+    private float cloudspeed = 0.01f;
+    private float birdspeed = 0.1f;
+    private void Update()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        clouds.transform.Translate(Vector2.left * cloudspeed * Time.deltaTime);
+        bird_L.transform.Translate(Vector2.left * birdspeed * Time.deltaTime);
+        bird_R.transform.Translate(Vector2.left * birdspeed * Time.deltaTime);
     }
 }
